d7b9050d3e3ad7eca773c09a67d1b8488b34b5be
Leonid Kulikov <leokul01@students.programming1189.ru>
2017-10-20 15:56:02 +0300

    Initial commit

--- program.s
+++ program.s
@@ -4,7 +4,7 @@ section .text
     global main  ; Export main for linker
     extern printf  ; Request linking with printf in linker
 sum:
-    ; Preable
+    ; Preamble
     push ebp
     mov ebp, esp
 
@@ -12,6 +12,33 @@ sum:
     mov eax, dword [ebp + 8]
     add eax, dword [ebp + 12]
 
+    ; Epilogue
+    pop ebp
+    ret
+discriminant:
+    ; Preamble
+    push ebp
+    mov ebp, esp
+    push esi
+    sub esp, 4
+
+    ; Body
+    mov eax, dword [ebp + 8] ; a
+    mov ecx, dword [ebp + 12] ; b
+    mov edx, dword [ebp + 16] ; c
+    mov esi, 4
+    mul esi  ; overwrites edx and eax
+    mov dword [esp], eax
+    mov eax, dword [ebp + 16]
+    mul dword [esp]
+    mov dword [esp], eax
+    mov eax, ecx
+    mul eax
+    mov ecx, dword [esp]
+    sub eax, ecx
+
+    add esp, 4
+    pop esi
     ; Epilogue
     pop ebp
     ret
@@ -23,12 +50,13 @@ main:
     push esi
     push edi
 
-    push dword 2  ; y
-    push dword 1  ; x
-    call sum
-    add esp, 8
+    push dword 3  ; c
+    push dword 2  ; b
+    push dword 1  ; a
+    call discriminant
+    add esp, 12
 
-    push eax  ; result of sum
+    push eax  ; result of discriminant
     push format
     call printf
     add esp, 8
